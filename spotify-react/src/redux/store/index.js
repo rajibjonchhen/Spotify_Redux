e044e52bc4a6c9ca3503bdfb5d1d27fcfcc33323
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-03-04 09:44:03 +0100

    added basix set up

---
+++ spotify-react/src/redux/store/index.js
@@ -0,0 +1,15 @@
+import {createStore} from 'redux'
+
+export const initialState = {
+    song:{
+        songs:[]
+    },
+    album:{
+        albums:[]
+    },
+    artist:{
+        artists:[]
+    }
+
+}
+
